a945dfa54f875785952173a2b785b8b3ad0f59be
meelivyas11 <meelivyas11@gmail.com>
2017-09-12 10:38:25 -0400

    new methods

---
+++ Problems/src/Practice3/CompareHashMaps.java
@@ -0,0 +1,10 @@
+package Practice3;
+
+public class CompareHashMaps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Problems/src/Practice3/SortTreeMapByValue.java
@@ -0,0 +1,6 @@
+package Practice3;
+
+public class SortTreeMapByValue {
+	
+	
+}
